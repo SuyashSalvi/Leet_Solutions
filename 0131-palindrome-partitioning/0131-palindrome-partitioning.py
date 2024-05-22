9764066a477653ee186006ddaf11ee5c8fd1b8fe
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-05-21 22:48:54 -0700

    Time: 459 ms (73.99%), Space: 35.2 MB (66.68%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,13 +1,22 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        def is_pali(s):
-            return s == s[::-1]
+        n = len(s)
+        dp = [[False]* n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+        for dif in range(1,n):
+            for i in range(n-dif):
+                j = i + dif
+                if s[i] == s[j]:
+                    if dif == 1 or dp[i+1][j-1] == True:
+                        dp[i][j] = True
+
         
-        def backtrack(start, path):
-            if start == len(s):
+        def backtrack(start,path):
+            if start == n:
                 res.append(path[:])
-            for end in range(start, len(s)):
-                if is_pali(s[start:end+1]):
+            for end in range(start,n):
+                if dp[start][end]:
                     backtrack(end+1,path+[s[start:end+1]])
         
         res = []
