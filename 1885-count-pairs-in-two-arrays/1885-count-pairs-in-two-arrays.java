87673529f499736457163c2afc7cb3c099cb072d
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-05-30 13:33:54 -0700

    Time: 26 ms (73.78%), Space: 59 MB (41.14%) - LeetHub

---
+++ 1885-count-pairs-in-two-arrays/1885-count-pairs-in-two-arrays.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long countPairs(int[] nums1, int[] nums2) {
+        // 0  6  5 -3 -> -3 0 5 6/ 6 5 0 -3
+        int n = nums1.length;
+        int[] diff = new int[n];
+        for(int i=0;i<n;i++){
+            diff[i] = nums1[i] - nums2[i];
+        }
+        
+        Arrays.sort(diff);
+        int last = diff.length - 1;
+        int first = 0;
+        long ans = 0;
+
+        while (last > first){
+
+            while (last > first && diff[last] + diff[first] <= 0){
+                first++;
+            }
+
+            if (last == first){
+                break;
+            } else {
+                ans = ans + last - first;
+                last--;
+            }
+
+        }
+
+        return ans;
+    }
+} 
