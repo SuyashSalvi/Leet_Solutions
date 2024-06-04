e2b2290f43c7723e0ebd056f611ac54afc6ebc96
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-06-04 12:12:13 -0700

    Time: 5 ms (66.65%), Space: 42.3 MB (5.05%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashSet<Character> hs = new HashSet<>();
+        int ans = 0;
+        
+        for(char c: s.toCharArray()){
+            if(hs.contains(c)){
+                hs.remove(c);
+                ans += 2;
+            } else{
+                hs.add(c);
+            }
+        }
+        if(!hs.isEmpty()){
+            ans += 1;
+        }
+        
+        return ans;
+    }
+}
