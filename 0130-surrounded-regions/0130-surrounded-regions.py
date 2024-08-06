22bc6dbe42d905109026fae62748e2a0231c6fc7
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-08-06 12:45:55 -0700

    Time: 117 ms (72.25%), Space: 17.9 MB (50.41%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        def bfs(r,c):
+            q = deque([(r,c)])
+            while q:
+                r, c = q.popleft()
+                if board[r][c] == 'O':
+                    board[r][c] = 'N'
+
+                    for dr, dc in[[r+1,c],[r,c+1],[r-1,c],[r,c-1]]:
+                        if (dr in range(len(board)) and dc in range(len(board[0])) and board[dr][dc]=="O"):
+                            q.append((dr, dc))
+                
+
+        m, n = len(board[0]), len(board)
+
+        for i in range(m):
+            if board[0][i] == "O":
+                bfs(0, i)
+            if board[n-1][i] == "O":
+                bfs(n-1, i)
+
+        for i in range(n):
+            if board[i][0] == "O":
+                bfs(i, 0)
+            if board[i][m-1] == "O":
+                bfs(i, m-1)
+
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == "X": 
+                    continue
+                elif board[i][j] == "N": 
+                    board[i][j] = "O"
+                else: board[i][j] = "X"
