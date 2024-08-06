97beb0f9529a286067a565249f274100b8c9015d
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-08-05 18:54:51 -0700

    Time: 34 ms (82.41%), Space: 16.5 MB (86.8%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,34 +1,22 @@
 class Solution(object):
     def myAtoi(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        ans = 0
-        i = 0
-        sign = 1
-        max_int = 2**31 - 1
-        min_int = -2**31
-        
         if len(s) == 0:
             return 0
-        
-        # Remove leading whitespaces
-        while i < len(s) and s[i] == ' ':
+
+        i, sign, ans = 0, 1, 0
+        max_int, min_int = 2**31 - 1, -2**31
+        while i < len(s) and s[i] == " ":
             i += 1
-        
-        # Check for sign
-        if i < len(s) and (s[i] == '-' or s[i] == '+'):
-            if s[i] == '-':
+
+        if i < len(s) and s[i] in "-+":
+            if s[i] == "-":
                 sign = -1
             i += 1
         
-        # Convert digits to integer
         while i < len(s) and '0' <= s[i] <= '9':
-            digit = ord(s[i]) - ord('0')
-            if ans > max_int // 10 or (ans == max_int // 10 and digit > max_int % 10):
+            d = ord(s[i]) - ord('0')
+            if ans > max_int // 10 or (ans == max_int // 10 and d > max_int % 10):
                 return max_int if sign == 1 else min_int
-            ans = ans * 10 + digit
+            ans = ans * 10 + d
             i += 1
-        
         return ans * sign
