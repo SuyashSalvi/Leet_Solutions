16b03462198e69a2af91c0ba6d0d663ac0ea1423
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-10-11 19:09:14 -0700

    Time: 52 ms (30.51%), Space: 17 MB (12.07%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,18 +1,36 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        if not p:
-            return not s
+        # if not p:
+        #     return not s
 
-        first_match = bool(s) and p[0] in {s[0],"."}
+        # first_match = bool(s) and p[0] in {s[0],"."}
 
-        if len(p) >= 2 and p[1] == "*":
-            return(
-                #zero present
-                self.isMatch(s, p[2:])
-                or
-                # more than zero present
-                first_match and self.isMatch(s[1:],p)
-            )
+        # if len(p) >= 2 and p[1] == "*":
+        #     return(
+        #         #zero present s[0]
+        #         self.isMatch(s, p[2:])
+        #         or
+        #         # more than zero present of s[0]
+        #         first_match and self.isMatch(s[1:],p)
+        #     )
 
-        else:
-            return first_match and self.isMatch(s[1:], p[1:])
+        # else:
+        #     return first_match and self.isMatch(s[1:], p[1:])
+
+
+        memo = {}
+
+        def dp(i: int,j: int) -> bool:
+            if(i,j) not in memo:
+                
+                if not bool(p[j:]): 
+                    ans = i == len(s)
+                else:
+                    first_match = bool(s[i:]) and p[j] in {s[i],"."}
+                    if bool(p[j+1:]) and p[j+1] == "*":
+                        ans = dp(i,j+2) or first_match and dp(i+1,j)
+                    else:
+                        ans = first_match and dp(i+1,j+1)
+                memo[i,j] = ans
+            return memo[i, j]
+        return dp(0,0) 
