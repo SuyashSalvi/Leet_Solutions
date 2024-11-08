9146f388ef7a029a8f065f55922ed2f4c2cf2bbf
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-11-08 12:03:44 -0800

    Time: 16 ms (46.3%), Space: 17.9 MB (89.91%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -8,17 +8,32 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         # return self.helper(grid,0,0)
 
+        # m = len(grid)
+        # n = len(grid[0])
+        # dp = [[0] * n for _ in range(m)]
+        # for i in range(m - 1, -1, -1):
+        #     for j in range(n -1, -1, -1):
+        #         if i == m - 1 and j != n - 1:
+        #             dp[i][j] = grid[i][j] + dp[i][j+1]
+        #         elif i != m - 1 and j == n - 1:
+        #             dp[i][j] = grid[i][j] + dp[i+1][j]
+        #         elif i != m - 1 and j != n - 1:
+        #             dp[i][j] = grid[i][j] + min(dp[i+1][j],dp[i][j+1])
+        #         else:
+        #             dp[i][j] = grid[i][j] 
+        # return dp[0][0]
+
         m = len(grid)
         n = len(grid[0])
-        dp = [[0] * n for _ in range(m)]
-        for i in range(m - 1, -1, -1):
+        dp = [[0] for _ in range(n)]
+        for i in range(len(grid) - 1, -1, -1):
             for j in range(n -1, -1, -1):
                 if i == m - 1 and j != n - 1:
-                    dp[i][j] = grid[i][j] + dp[i][j+1]
+                    dp[j] = grid[i][j] + dp[j+1]
                 elif i != m - 1 and j == n - 1:
-                    dp[i][j] = grid[i][j] + dp[i+1][j]
+                    dp[j] = grid[i][j] + dp[j]
                 elif i != m - 1 and j != n - 1:
-                    dp[i][j] = grid[i][j] + min(dp[i+1][j],dp[i][j+1])
+                    dp[j] = grid[i][j] + min(dp[j],dp[j+1])
                 else:
-                    dp[i][j] = grid[i][j] 
-        return dp[0][0]
+                    dp[j] = grid[i][j] 
+        return dp[0]
