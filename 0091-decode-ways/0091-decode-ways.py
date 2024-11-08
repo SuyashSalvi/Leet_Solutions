007192a484c63ab866486058e585b795543a60c9
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-11-08 13:52:27 -0800

    Time: 0 ms (100%), Space: 16.7 MB (46.81%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -18,15 +18,29 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         # return self.recursiveMemo(0, s)
 
-        dp = [0 for _ in range(len(s)+1)]
-        dp[0] = 1
-        dp[1] = 0 if s[0] == '0' else 1
+        # dp = [0 for _ in range(len(s)+1)]
+        # dp[0] = 1
+        # dp[1] = 0 if s[0] == '0' else 1
 
-        for i in range(2, len(dp)):
-            if s[i-1] != '0':
-                dp[i] = dp[i-1]
+        # for i in range(2, len(dp)):
+        #     if s[i-1] != '0':
+        #         dp[i] = dp[i-1]
 
-            two_digit = int(s[i-2:i])
+        #     two_digit = int(s[i-2:i])
+        #     if two_digit >= 10 and two_digit <= 26:
+        #         dp[i] += dp[i-2]    
+        # return dp[len(s)]
+
+        if s[0] == '0': return 0
+        two, one = 1, 1
+        for i in range(1, len(s)):
+            cur = 0
+            if s[i] != '0':
+                cur = one
+            two_digit = int(s[i-1:i+1])
             if two_digit >= 10 and two_digit <= 26:
-                dp[i] += dp[i-2]    
-        return dp[len(s)]
+                cur += two
+            two = one
+            one = cur
+        return one
+            
