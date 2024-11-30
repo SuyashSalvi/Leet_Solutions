bedff48321a84755543472fda924f624ef0f7179
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-11-30 13:39:45 -0800

    Time: 463 ms (14.03%), Space: 154.6 MB (5.78%) - LeetHub

--- 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.py
+++ 2201-valid-arrangement-of-pairs/2201-valid-arrangement-of-pairs.py
@@ -3,30 +3,20 @@ from collections import defaultdict, deque, Counter
 class Solution:
     def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
         # 1 build graph and track in and out degrees
-        adj_mat = defaultdict(deque)
-        inD = Counter()
-        outD = Counter()
+        adj_mat, inD, outD, res = defaultdict(deque), Counter(), Counter(), []
         for s,e in pairs:
             adj_mat[s].append(e)
             outD[s] += 1
             inD[e] += 1
 
         # 2 find start node
-        start_node = pairs[0][0]
-        for node in outD:
-            if inD[node] == outD[node] - 1:
-                start_node = node
-                break
+        start_node = next((node for node in outD if outD[node] == inD[node]+1),pairs[0][0])
 
         # 3 performing postorder DFS
-        res = []
         def dfs(node):
             while adj_mat[node]:
-                next_node = adj_mat[node].popleft()
-                dfs(next_node)
+                dfs(adj_mat[node].popleft())
             res.append(node)
 
         dfs(start_node)
-        res.reverse()
-
-        return [[res[i],res[i+1]] for i in range(len(res)-1)]
+        return [[res[i],res[i-1]] for i in range(len(res)-1,0,-1)]
