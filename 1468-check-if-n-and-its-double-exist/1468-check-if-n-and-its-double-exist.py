82e3d0ee3ae6043d8d0a6ebc12eb09985bb773ff
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-12-01 10:52:54 -0800

    Time: 11 ms (25.66%), Space: 17.4 MB (5.25%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,6 +1,5 @@
 from collections import Counter
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        count = Counter(arr)
-        return any((num != 0 and 2 * num in count) or (num == 0 and count[num] > 1) for num in arr)
+        return any((num != 0 and 2 * num in Counter(arr)) or (num == 0 and Counter(arr)[num] > 1) for num in arr)
  
