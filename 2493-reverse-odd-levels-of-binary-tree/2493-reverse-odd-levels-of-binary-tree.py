b5e88db0b8651df9b954fb686b24626a34974ac5
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-12-19 22:38:36 -0800

    Time: 98 ms (6.38%), Space: 22.9 MB (5.08%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
@@ -6,14 +6,26 @@
 #         self.right = right
 class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def dfs(left_child, right_child, level):
-            if left_child == None or right_child == None:
-                return
-            # if the next level is odd, need to interchange their node values
-            if level % 2 == 0:
-                left_child.val, right_child.val = right_child.val, left_child.val
-            dfs(left_child.left, right_child.right, level + 1)
-            dfs(left_child.right, right_child.left, level + 1)
+        q = [root]
+        level = 0
 
-        dfs(root.left, root.right, 0)
+        while q:
+            cur_q_size = len(q)
+            cur_level_nodes = []
+
+            for _ in range(cur_q_size):
+                node = q.pop(0)
+                if level % 2 == 1:
+                    cur_level_nodes.append(node)
+                if node.left:
+                    q.append(node.left)
+                    q.append(node.right)
+                
+            if level % 2 == 1:
+                left, right = 0, len(cur_level_nodes) - 1
+                while left < right:
+                    cur_level_nodes[left].val, cur_level_nodes[right].val = cur_level_nodes[right].val, cur_level_nodes[left].val
+                    left += 1
+                    right -= 1
+            level += 1
         return root
