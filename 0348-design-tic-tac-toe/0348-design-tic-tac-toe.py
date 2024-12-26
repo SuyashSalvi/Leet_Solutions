01980553376f0d0337d5555e45d7fedf3f589714
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2024-12-26 02:13:10 -0800

    Time: 84 ms (23.81%), Space: 20.3 MB (6.39%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
@@ -1,38 +1,26 @@
 class TicTacToe:
 
     def __init__(self, n: int):
-        self.board = [[0] * n for _ in range(n)] 
+        self.rows = [0] * n
+        self.cols = [0] * n
+        self.dia = 0
+        self.antidia = 0
         self.n = n
 
-    def check_row(self, row, player):
-        for col in range(self.n):
-            if self.board[row][col] != player:
-                return False
-        return True
-
-    def check_col(self, col, player):
-        for row in range(self.n):
-            if self.board[row][col] != player:
-                return False
-        return True
-    
-    def check_dia(self, player):
-        for row in range(self.n):
-            if self.board[row][row] != player:
-                return False
-        return True
-
-    def check_antidia(self, player):
-        for row in range(self.n):
-            if self.board[row][self.n - row - 1] != player:
-                return False
-        return True
-
     def move(self, row: int, col: int, player: int) -> int:
-        self.board[row][col] = player
-        if (self.check_row(row,player) or self.check_col(col, player) or (row == col and self.check_dia(player)) or (row == self.n - col - 1 and self.check_antidia(player))):
+        cur_player = 1 if player == 1 else -1
+        self.rows[row] += cur_player
+        self.cols[col] += cur_player
+
+        if row == col:
+            self.dia += cur_player
+        if col == self.n - row - 1:
+            self.antidia += cur_player
+
+        if (abs(self.rows[row]) == self.n or abs(self.cols[col]) == self.n or abs(self.dia) == self.n or abs(self.antidia) == self.n):
             return player
-        return 0
+
+        return 0    
 
 
 # Your TicTacToe object will be instantiated and called as such:
