4bdd7c90dc862f50bcc35576d44d9bb6fb2bad32
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2025-01-23 15:23:32 -0800

    Time: 147 ms (32%), Space: 17.8 MB (64%) - LeetHub

--- 2062-game-of-nim/2062-game-of-nim.py
+++ 2062-game-of-nim/2062-game-of-nim.py
@@ -1,25 +1,29 @@
 class Solution:
     def nimGame(self, piles: List[int]) -> bool:
-        remaining = sum(piles)
-        memo = {}
-        return self.__is_next_person_winner(piles, remaining, memo)
+        def __game(piles, remaining,memo):
+            key = '-'.join(map(str,piles))
+            if key in memo:
+                return memo[key]
+            
+            if remaining == 0:
+                return False
 
-    def __is_next_person_winner(self, piles, remaining, memo):
-        key = "-".join(map(str, piles))
-
-        if key in memo:
-            return memo[key]
-
-        if remaining == 0:
+            # going through all the states
+            for i in range(len(piles)):
+                # considering all the permutations
+                for j in range(1, piles[i]+1):
+                    piles[i] -= j
+                    
+                    next_state = sorted(piles)
+                    if not __game(next_state,remaining-j,memo):
+                        memo[key] = True
+                        return True
+                    piles[i] += j
+            memo[key] = False
             return False
 
-        for i in range(len(piles)):
-            for j in range(1, piles[i] + 1):
-                piles[i] -= j
-                next_state = sorted(piles)
-                if not self.__is_next_person_winner(next_state, remaining - j, memo):
-                    memo[key] = True
-                    return True
-                piles[i] += j
-        memo[key] = False
-        return False
+        remaining = sum(piles)
+        memo = {}
+        return __game(piles,remaining,memo)
+    
+        
