a0a4bc94030c48ba1e3490789a402d5beea630df
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2025-01-23 23:50:11 -0800

    Time: 23 ms (74.12%), Space: 18.2 MB (16.08%) - LeetHub

---
+++ 2078-maximum-compatibility-score-sum/2078-maximum-compatibility-score-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxCompatibilitySum(self, students: List[List[int]], mentors: List[List[int]]) -> int:
+        m, n = len(students), len(students[0])
+
+        @functools.cache
+        def dp(student, mentor):
+            if student == m:
+                return 0
+            
+            res = 0
+            for i in range(m):
+                if mentor & (1 << i): continue
+                score = sum(1 for j in range(n) if students[student][j] == mentors[i][j])
+                res = max(res, score + dp(student + 1, mentor | (1 << i)))
+
+            return res
+        return dp(0, 0)
