f4d9362347230bbe683295d668dcf1b48135a7de
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2025-01-27 20:43:44 -0800

    Time: 3 ms (10.81%), Space: 17.7 MB (57.08%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -3,7 +3,7 @@ class Solution:
         prev_row = [1]
 
         for row_num in range(1, rowIndex + 1):
-            cur_row = [1] * (row_num + 1)
+            cur_row = [1 for _ in range(row_num + 1)]
 
             for i in range(1, row_num):
                 cur_row[i] = prev_row[i - 1] + prev_row[i]
