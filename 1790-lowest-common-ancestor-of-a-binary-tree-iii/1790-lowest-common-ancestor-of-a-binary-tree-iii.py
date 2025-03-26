96b453d84244694478948d1d71a1a4d1e84e71cf
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2025-03-25 22:09:45 -0700

    Time: 59 ms (23.42%), Space: 20.9 MB (82.53%) - LeetHub

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -9,32 +9,10 @@ class Node:
 """
 
 class Solution:
-    # def get_depth(self, p):
-    #     depth = 0
-    #     while p:
-    #         p = p.parent
-    #         depth += 1
-    #     return depth
-
-
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-    #     pd = self.get_depth(p)
-    #     qd = self.get_depth(q)
+        a, b = p, q
+        while a != b:
+            a = a.parent if a else q
+            b = b.parent if b else p
 
-    #     for _ in range(pd - qd):
-    #         p = p.parent
-    #     for _ in range(qd - pd):
-    #         q = q.parent
-
-    #     while p != q:
-    #         p = p.parent
-    #         q = q.parent
-        
-    #     return p
-
-        p1, p2 = q, p
-        while p1 != p2:
-            p1 = p1.parent if p1.parent else q
-            p2 = p2.parent if p2.parent else p
-
-        return p1
+        return a
