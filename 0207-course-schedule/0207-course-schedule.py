986dbffba3c1069d8a65cb265cd91fbdbfb11dea
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2025-03-30 09:58:43 -0700

    Time: 3 ms (88.92%), Space: 19.1 MB (62.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,23 @@
 class Solution:
-    def dfs(self, i, adj, inStack, visited):
-        if inStack[i]:
-            return True
-        if visited[i]:
-            return False
-        visited[i] = True
-        inStack[i] = True
-        for neigh in adj[i]:
-            if self.dfs(neigh, adj, inStack, visited):
-                return True
-        inStack[i] = False
-        return False
-
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegree = [0] * numCourses
         adj = [[] for _ in range(numCourses)]
         for prereq in prerequisites:
             adj[prereq[1]].append(prereq[0])
+            indegree[prereq[0]] += 1
 
-        visited = [False] * numCourses
-        inStack = [False] * numCourses
-
+        visited = 0
+        q = deque()
         for i in range(numCourses):
-            if self.dfs(i, adj, inStack, visited):
-                return False
-        return True
+            if indegree[i] == 0:
+                q.append(i)
+        
+        while q:
+            node = q.popleft()
+            visited += 1
+
+            for neigh in adj[node]:
+                indegree[neigh] -= 1
+                if indegree[neigh] == 0:
+                    q.append(neigh)
+        return visited == numCourses
