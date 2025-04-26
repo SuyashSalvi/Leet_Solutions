90c991afaabddb31164da3a903bb59cddf766506
Suyash Salvi <40499151+SuyashSalvi@users.noreply.github.com>
2025-04-25 17:15:53 -0700

    Time: 147 ms (86.58%), Space: 32 MB (24.99%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,21 +1,32 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # monotonically decreasing dq
-        dq = deque()
+        """
+        Return a list of the maximums of each sliding window of size k.
+        Uses a monotonic deque to achieve O(n) time.
+        """
+        dq = deque()  # will store indices, values in decreasing order
         res = []
 
+        # Build initial window
         for i in range(k):
             while dq and nums[i] >= nums[dq[-1]]:
                 dq.pop()
             dq.append(i)
-
         res.append(nums[dq[0]])
+
+        # Slide the window
         for i in range(k, len(nums)):
-            if dq and dq[0] == i - k:
+            # Evict indices out of this window
+            if dq and dq[0] < i - k + 1:
                 dq.popleft()
+            # Maintain decreasing order in deque
             while dq and nums[i] >= nums[dq[-1]]:
                 dq.pop()
             dq.append(i)
+            # Front is the max for this window
             res.append(nums[dq[0]])
 
         return res
